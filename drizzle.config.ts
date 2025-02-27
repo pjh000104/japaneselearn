413253267d17739a724f294309615b7db75af9ed
Jungho Park <122625228+pjh000104@users.noreply.github.com>
2025-02-26 19:47:09 -0500

    Changed vairables to env vairables

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   schema: "./src/lib/db/schema.ts",
   out: "./migrations",
   dbCredentials: {
-    url: "libsql://japanese-words-pjh000104.turso.io",
-    authToken: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MzkxNTE0ODIsImlkIjoiMjNkZDY0NzktMzNkMC00YWVlLWE2OTMtNTAyOTQ1YmFlMjY5In0.3xB-15AC-QYAwApDo69flc9wSQ2JolLxdIAbTxeRjlLHSu63EBjnpLWVHNRrM8-4HgKwMcbBPm0qe1Zu6zY1Dg",
+    url: process.env.TURSO_DATABASE_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
   },
 });
